5825a7dcdf1925dd04a5fd8067d942da50fe85f7
diegooo01 <criollodiego542@gmail.com>
2024-06-07 00:10:30 -0500

    feat: add CategoryOngQueryService implementation
    I've added the CategoryOngQueryService implementation into queryservices

--- src/main/java/techzo/com/example/cambiazo/donations/application/internal/queryservices/CategoryOngQueryServiceImpl.java
+++ src/main/java/techzo/com/example/cambiazo/donations/application/internal/queryservices/CategoryOngQueryServiceImpl.java
@@ -1,4 +1,30 @@
 package techzo.com.example.cambiazo.donations.application.internal.queryservices;
 
-public class CategoryOngQueryServiceImpl {
+import org.springframework.stereotype.Service;
+import techzo.com.example.cambiazo.donations.domain.model.aggregates.CategoryOng;
+import techzo.com.example.cambiazo.donations.domain.model.queries.GetAllCategoryOngsQuery;
+import techzo.com.example.cambiazo.donations.domain.model.queries.GetCategoryOngByIdQuery;
+import techzo.com.example.cambiazo.donations.domain.services.CategoryOngQueryService;
+import techzo.com.example.cambiazo.donations.infrastructure.persistence.jpa.CategoryOngRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryOngQueryServiceImpl implements CategoryOngQueryService {
+    private final CategoryOngRepository categoryOngRepository;
+
+    public CategoryOngQueryServiceImpl(CategoryOngRepository categoryOngRepository) {
+        this.categoryOngRepository = categoryOngRepository;
+    }
+
+    @Override
+    public List<CategoryOng> handle(GetAllCategoryOngsQuery query) {
+        return categoryOngRepository.findAll();
+    }
+
+    @Override
+    public Optional<CategoryOng> handle(GetCategoryOngByIdQuery query) {
+        return categoryOngRepository.findById(query.id());
+    }
 }
