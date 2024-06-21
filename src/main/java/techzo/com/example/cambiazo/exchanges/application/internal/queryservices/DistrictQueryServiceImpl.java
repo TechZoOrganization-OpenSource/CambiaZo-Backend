4ea0eaacfdf97d31fe734c9d623c561caf8ccde6
123-Mathias <u202216282@upc.edu.pe>
2024-06-21 01:26:50 -0500

    feat: adding the department query service impl
    I've added the district query service implementation

---
+++ src/main/java/techzo/com/example/cambiazo/exchanges/application/internal/queryservices/DistrictQueryServiceImpl.java
@@ -0,0 +1,45 @@
+package techzo.com.example.cambiazo.exchanges.application.internal.queryservices;
+
+import org.springframework.stereotype.Service;
+import techzo.com.example.cambiazo.exchanges.domain.exceptions.DepartmentNotFoundException;
+import techzo.com.example.cambiazo.exchanges.domain.model.aggregates.Department;
+import techzo.com.example.cambiazo.exchanges.domain.model.aggregates.District;
+import techzo.com.example.cambiazo.exchanges.domain.model.queries.GetAllDistrictsByDepartmentIdQuery;
+import techzo.com.example.cambiazo.exchanges.domain.model.queries.GetAllDistrictsQuery;
+import techzo.com.example.cambiazo.exchanges.domain.model.queries.GetDistrictByIdQuery;
+import techzo.com.example.cambiazo.exchanges.domain.services.DistrictQueryService;
+import techzo.com.example.cambiazo.exchanges.infrastructure.persistence.jpa.DepartmentRepository;
+import techzo.com.example.cambiazo.exchanges.infrastructure.persistence.jpa.DistrictRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class DistrictQueryServiceImpl implements DistrictQueryService {
+
+    private final DistrictRepository districtRepository;
+
+    private final DepartmentRepository departmentRepository;
+
+    public DistrictQueryServiceImpl(DistrictRepository districtRepository, DepartmentRepository departmentRepository) {
+        this.districtRepository = districtRepository;
+        this.departmentRepository = departmentRepository;
+    }
+
+    @Override
+    public List<District> handle(GetAllDistrictsQuery query) {
+        return districtRepository.findAll();
+    }
+
+    @Override
+    public List<District>handle(GetAllDistrictsByDepartmentIdQuery query){
+        Department department = departmentRepository.findById(query.departmentId())
+                .orElseThrow(() -> new DepartmentNotFoundException(query.departmentId()));
+        return districtRepository.findAllDistrictsByDepartmentId(department);
+    }
+
+    @Override
+    public Optional<District>handle(GetDistrictByIdQuery query){
+        return districtRepository.findById(query.id());
+    }
+}
