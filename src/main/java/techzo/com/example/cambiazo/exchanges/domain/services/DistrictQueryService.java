a867e63be79e8544002094a1f34cb753aaa6c7fd
HelloNerk <josephhuamanilol@gmail.com>
2024-06-25 18:09:37 -0500

    feat: Adding get method get district by name
    I've added a get method that get district by name

--- src/main/java/techzo/com/example/cambiazo/exchanges/domain/services/DistrictQueryService.java
+++ src/main/java/techzo/com/example/cambiazo/exchanges/domain/services/DistrictQueryService.java
@@ -4,6 +4,7 @@ import techzo.com.example.cambiazo.exchanges.domain.model.aggregates.District;
 import techzo.com.example.cambiazo.exchanges.domain.model.queries.GetAllDistrictsByDepartmentIdQuery;
 import techzo.com.example.cambiazo.exchanges.domain.model.queries.GetAllDistrictsQuery;
 import techzo.com.example.cambiazo.exchanges.domain.model.queries.GetDistrictByIdQuery;
+import techzo.com.example.cambiazo.exchanges.domain.model.queries.GetDistrictByNameQuery;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,6 +14,8 @@ public interface DistrictQueryService {
     Optional<District>handle(GetDistrictByIdQuery query);
 
     List<District> handle(GetAllDistrictsQuery query);
+
+    Optional<District> handle(GetDistrictByNameQuery query);
 
     List<District>handle(GetAllDistrictsByDepartmentIdQuery query);
 }
--- src/main/java/techzo/com/example/cambiazo/exchanges/infrastructure/persistence/jpa/DistrictRepository.java
+++ src/main/java/techzo/com/example/cambiazo/exchanges/infrastructure/persistence/jpa/DistrictRepository.java
@@ -3,9 +3,13 @@ package techzo.com.example.cambiazo.exchanges.infrastructure.persistence.jpa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import techzo.com.example.cambiazo.exchanges.domain.model.aggregates.Department;
 import techzo.com.example.cambiazo.exchanges.domain.model.aggregates.District;
+import techzo.com.example.cambiazo.exchanges.domain.model.valueobjects.DistrictName;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface DistrictRepository extends JpaRepository<District, Long> {
     List<District>findAllDistrictsByDepartmentId(Department id);
+
+    Optional<District>findByName(DistrictName name);
 }
